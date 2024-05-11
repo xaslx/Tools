880ce8e92307266481a8f15f3da9838a09aabba7
maksim <knyazev.maksim2852@gmail.com>
2024-05-11 16:26:26 +0400

    update yt router

--- routers/youtube_video_downloader.py
+++ routers/youtube_video_downloader.py
@@ -6,7 +6,7 @@ from fastapi.templating import Jinja2Templates
 
 from pytube.exceptions import RegexMatchError, VideoUnavailable
 from io import BytesIO
-
+from tools import Tools
 
 templates = Jinja2Templates('static/templates')
 
@@ -24,24 +24,7 @@ async def get_main_page(request: Request):
 
 
 @router.post('', response_class=StreamingResponse)
-async def download_video(link: str = Form(...), resolution: str = Form(...)):
-    try:
-        yt: YouTube = YouTube(link)
-    except (RegexMatchError, VideoUnavailable) as e:
-        raise HTTPException(status_code=404, detail='Видео не найдено или недоступно!')
-
-    if resolution == 'highest':
-        video = yt.streams.get_highest_resolution()
-    elif resolution == 'lowest':
-        video = yt.streams.get_lowest_resolution()
-
-    if video.filesize > 524288000:
-        raise HTTPException(status_code=413, detail='Видео не может быть больше 500мб')
-    
-    buffer: BytesIO = BytesIO()
-    video.stream_to_buffer(buffer)
-    buffer.seek(0)
-
-    return StreamingResponse(iter([buffer.getvalue()]), media_type='video/mp4')
+async def download_video(link: str = Form(), resolution: str = Form()):
+    return await Tools.download_video_yt(link, resolution)
 
 
