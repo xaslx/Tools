37b1c11e4f59af98999692cb52935ff343c1c2ea
maksim <knyazev.maksim2852@gmail.com>
2024-05-11 16:25:17 +0400

    update tiktok router

--- routers/tiktok_video_downloader.py
+++ routers/tiktok_video_downloader.py
@@ -1,16 +1,12 @@
-from fastapi import APIRouter, Request, Form, Body
-from fastapi.responses import FileResponse, StreamingResponse
-from fastapi.exceptions import HTTPException
+from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
-from tiktok_downloader import ttdownloader
-import secrets
-from io import BytesIO, BufferedWriter
+from tools import Tools
 
 templater = Jinja2Templates('static/templates')
 
 router = APIRouter(
     prefix='/download/tiktok',
-    tags=['Загрузчик видео с тик ток']
+    tags=['Загрузчик видео с Тик Ток']
 )
 
 
@@ -19,13 +15,5 @@ async def get_main_page(request: Request):
     return templater.TemplateResponse('tiktok.html', {'request': request})
 
 @router.post('')
-async def download_video(link: str = Form(...)):
-    try:
-        video = ttdownloader(link)
-    except ValueError:
-        raise HTTPException(status_code=404, detail='Видео не найдено, или неверная ссылка')
-    buffer = BytesIO()
-    writer = BufferedWriter(buffer)
-    video[0].download(writer)
-    buffer.seek(0)
-    return StreamingResponse(iter([buffer.getvalue()])) 
+async def download_video(link: str = Form()):
+    return await Tools.download_video_tt(link=link)
